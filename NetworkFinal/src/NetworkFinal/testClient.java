c4569499f66c763c7bbd5740bf946d43c035a3b1
guo <b22790188@gmail.com>
2022-12-17 23:50:51 +0800

    Commit only needed file.

---
+++ NetworkFinal/src/NetworkFinal/testClient.java
@@ -0,0 +1,13 @@
+package NetworkFinal;
+
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+public class testClient {
+				
+	public static void main(String[] args) {
+		Client c = new Client();
+		c.sendDisconnectRequest();
+	}
+}
